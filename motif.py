6d05af5f914fe4e57286304ac1d5c0f58b3982df
B198096-2022 <email_not_provided>
2022-11-10 14:59:19 +0000

    motif step on full dict: only does first?

--- motif.py
+++ motif.py
@@ -1,10 +1,15 @@
 #!/bin/python3
-#This code splits up the protein seqiences into fasta headers and sequences
-#Puts a "_" to deliminate the sequences
-#Open the files in python then split them into a list
-#Then put them into a dict with header as key and seq as value
-grep ">" txid4890.prot.fa > headers.txt
-awk '/^>/ { print "_"; next; }; {print; }' txid4890.prot.fa > seqs.txt
+import os
+import shutil
+
+taxon = "txid4890"
+
+#The awk command doesn't work in this format
+headercommand = "grep '>' "+taxon+".prot.fa > headers.txt"
+seqscommand = "awk '/^>/ { print '_'; next; }; {print; }' "+taxon+".prot.fa > seqs.txt"
+os.system(headercommand)
+os.system(seqscommand)
+
 
 with open("headers.txt") as file:
     headers = file.read()
@@ -12,20 +17,19 @@ with open("headers.txt") as file:
 with open("seqs.txt") as file:
     seqs = file.read()
 
-seqclean = seqs.replace("\n", "")
-seqlist = seqclean.split("_")
+seqlist = seqs.split("_")
 headerlist = headers.split("\n")
 if seqlist[0] == '':
-    seqlist = seqlist[1:]
+    seqlist = seqlist[1:] #This DOES cut off first empty space
 
 if seqlist[-1] == '':
-    seqlist = seqlist[0:-2]
+    seqlist = seqlist[0:-1] #This does nothing
 
 if headerlist[0] == '':
-    headerlist = headerlist[1:]
+    headerlist = headerlist[1:] #This does nothing
 
 if headerlist[-1] == '':
-    headerlist = headerlist[0:-2]
+    headerlist = headerlist[0:-1] #This cuts off the final empty space
 
 dictlen = len(seqlist)
 
@@ -34,15 +38,13 @@ seqdict[headerlist[0]] = seqlist[0]
 for i in range(dictlen):
     seqdict[headerlist[i]] = seqlist[i]
 
-#Make a test dictionary with the first sequence
-testdict = {}
-testdict[headerlist[0]] = seqlist[0]
-testdict[headerlist[1]] = seqlist[1]
 
-#Making individual fasta files works better!
+#Thinking I'll need to make an actual fasta file and then read in the file
+os.mkdir("./patmatmotifs")
+os.mkdir("./indifasta")
 
-for head, seq in testdict.items():
-    fasta = name+ os.linesep +seq
+for head, seq in seqdict.items():
+    fasta = head+seq
     headline = head.split()
     code = (headline[0])
     code = code[1:-3]
@@ -50,3 +52,5 @@ for head, seq in testdict.items():
          my_file.write(fasta)
     command = "patmatmotifs -full -sequence "+code+".fa -sprotein1 YES -sformat1 fasta -outfile "+code+".patmatmotifs"
     os.system(command)
+    shutil.move("./"+code+".patmatmotifs", "./patmatmotifs/"+code+".patmatmotifs")
+    shutil.move("./"+code+".fa", "./indifasta/"+code+".fa")
